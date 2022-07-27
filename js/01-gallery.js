4327411314eae90d33b0682d82ad437dfda17784
Maryshchyn <maryshchyn89@gmail.com>
2022-07-27 15:44:18 +0300

    323

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,93 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createElementsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+function createElementsMarkup(galleryItems) {
+    return galleryItems
+        .map(({ original, preview, description }) => {
+            return `
+            <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+             <img
+             class="gallery__image"
+             src="${preview}"
+             data-source="${original}"
+             alt="${description}"
+             />
+            </a>
+            </div>`;
+        }).join("")
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// galleryContainer.addEventListener("click", selectGallery);
+
+//     const instance = basicLightbox.create(`
+// <img class="gallery__link" src="${selectGallery}">`, {
+//         onShow: (instance) => {
+//            window.addEventListener("keydown", handClick);
+//         },
+//         onClose: (instance) => {
+//            window.removeEventListener("keydown", handClick);
+//         }
+//     })
+
+// function selectGallery(event) {
+//     event.preventDefault();
+//     const selectGallery = event.target.dataset.source;
+
+//     if (event.target.className !== "gallery__image") {
+//         console.log(event.target.className);
+//         return;
+//     }
+//     instance.show();  
+//     instance.element().querySelector(".gallery__link").src = selectGallery;
+    
+//     };
+
+    
+//    function handClick(evt) {
+//         if (evt.keyCode === 27) {
+//             instance.close()
+//             return;
+//         }
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
