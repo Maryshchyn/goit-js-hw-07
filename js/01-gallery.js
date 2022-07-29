0557a3a2116baf0149586b3e413bf3eaeeb68f4c
Maryshchyn <maryshchyn89@gmail.com>
2022-07-29 16:03:21 +0300

    3333

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,17 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 const galleryContainer = document.querySelector(".gallery");
 const galleryMarkup = createElementsMarkup(galleryItems);
-galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
-
-galleryContainer.addEventListener("click", selectGallery);
-
-function selectGallery(evt) {
-    evt.preventDefault();
-}
-
-
 function createElementsMarkup(galleryItems) {
     return galleryItems
         .map(({ original, preview, description }) => {
@@ -28,28 +18,38 @@ function createElementsMarkup(galleryItems) {
             </div>`;
         }).join("")
 };
-
-
-const instance = basicLightbox.create(`<img
-             class="gallery__image"
-             src="${preview}"
-             data-source="${original}"
-             alt="${description}"
-             />`, {
-    onShow: (instance) => {
-        window.addEventListener("keydown", openClose);
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+function selectGallery(event) {
+    console.log(event.target);
+    console.log(event.currentTarget);
+    event.preventDefault();
+    if(event.target.nodeName !== 'IMG'){
+        console.log(event.target.className);
+        return;}
+const instance = basicLightbox.create( `<div class="modal">
+<img src = ${event.target.dataset.source}
+  alt=${event.target.alt}
+  width = '1280'
+  height = '800'
+/>
+</div> `, {
+    onShow: instance => {
+        console.log('add Listener');
+        instance.element().querySelector('img').onclick = instance.close;
+              window.addEventListener("keydown", handleEscape);
     },
-    onClose: (instance) => {
-        window.removeEventListener("keydown", openClose);
+    onClose: instance => {
+        console.log('remove Listener');
+        window.removeEventListener("keydown", handleEscape);
     }
 });
-
-function openClose(evt) {
-    if (evt.target.className !== "gallery__image") {
-        console.log(evt.target.className);
-        return;
+function handleEscape(event) {
+    if (event.key === 'Escape') {
+        console.log('esc');
+      instance.close();
+      return;
     }
-}
-
-
-
+  }
+  instance.show();
+};
+    galleryContainer.addEventListener("click", selectGallery);
